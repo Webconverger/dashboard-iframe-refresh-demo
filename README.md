02b768cd89893d0414348662a099c0910ef38377
Kai Hendry <hendry@webconverger.com>
2015-08-06 14:48:10 +0800

    Flex !

---
+++ README.md
@@ -0,0 +1,1 @@
+<img src=http://s.natalian.org/2015-08-06/1438843651_958x548.png alt="Flexbox dashboad">
--- index.php
+++ index.php
@@ -4,22 +4,24 @@
 <meta charset="utf-8" />
 <title>Dashboard example</title>
 <style>
+h1 { text-align: center; }
 html { font-family: Helvetica, Arial, Sans-Serif; font-size: large; height: 100%; }
 body { display: flex; width: 100%; height: 100%; flex-direction: column;}
-body > * { flex: 1; flex-grow: 1; }
+body > * { flex: 1; }
 .box { display: flex; }
-.box iframe { flex-grow: 1; overflow: hidden; }
+.box iframe { flex: 1; }
 </style>
 </head>
 <body>
 
-<h1><?php echo date('r'); ?></h1>
+<h1>Entire page loaded at <?php echo date('r'); ?></h1>
 <iframe id="foo" src="/time"></iframe>
 <div class=box>
 <iframe src="http://www.bbc.com/news"></iframe>
 <iframe src="https://webconverger.com/"></iframe>
 </div>
-<p>Simply showing how to reload the entire page after 10 seconds. As well as refreshing an iframe every second.</p>
+
+<p>Simply showing how to reload the entire page after 10 seconds. As well as refreshing an iframe every second via Javascript.</p>
 
 <script>
 window.setInterval("reloadIFrame();", 1000);
