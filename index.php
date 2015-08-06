06d1092e22b85d18ed7d63bdb87c9ac009115a22
Kai Hendry <hendry@webconverger.com>
2015-08-06 14:40:54 +0800

    Flexbox!

--- index.php
+++ index.php
@@ -4,13 +4,23 @@
 <meta charset="utf-8" />
 <title>Dashboard example</title>
 <style>
-html, body { height: 100%; font-family: Helvetica, Arial, Sans-Serif; font-size: large;  }
-#foo { width: 100%; height: 30%; }
+html { font-family: Helvetica, Arial, Sans-Serif; font-size: large; height: 100%; }
+body { display: flex; width: 100%; height: 100%; flex-direction: column;}
+body > * { flex: 1; flex-grow: 1; }
+.box { display: flex; }
+.box iframe { flex-grow: 1; overflow: hidden; }
 </style>
 </head>
 <body>
+
 <h1><?php echo date('r'); ?></h1>
 <iframe id="foo" src="/time"></iframe>
+<div class=box>
+<iframe src="http://www.bbc.com/news"></iframe>
+<iframe src="https://webconverger.com/"></iframe>
+</div>
+<p>Simply showing how to reload the entire page after 10 seconds. As well as refreshing an iframe every second.</p>
+
 <script>
 window.setInterval("reloadIFrame();", 1000);
 window.setInterval("location.reload();", 10000);
@@ -19,10 +29,6 @@ function reloadIFrame(){
 	el.src = el.src;
 }
 </script>
-
-<p>Simply showing how to reload the entire page after 10 seconds. As well as refreshing an iframe every second.</p>
-
-
 
 </body>
 </html>
